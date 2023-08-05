88088b83ff3fc6177b720a7ee0505b1ad0f8880a
James <kxxfiv@gmail.com>
2023-08-05 08:31:49 +0800

    change

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -14,4 +14,29 @@ const registerRouter = require('./register');
 router.use('/login', loginRouter);
 router.use('/register', registerRouter);
 
+
+// 验证输入格式，特别是密码格式和长度之类的
+function validateReqBody(req) {
+  const { id, password } = req.body;
+
+  if (!id || typeof id !== 'string' || !password || typeof password !== 'string') 
+  {
+    return false;
+  }
+  return true;
+}
+
+
+// 验证JWT是否有效
+function authenticateToken(req, res, next) {
+const token = req.header('Authorization')?.split(' ')[1];
+if (!token) return res.status(401).json({ error: '未登录' });
+
+jwt.verify(token, 'csdn1acb8ck2sd0fs', (err, id) => {
+  if (err) return res.status(403).json({ error: '验证失败' });
+  req.user = id;
+  next();
+});
+}
+
 module.exports = router;
